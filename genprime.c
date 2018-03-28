46ad1ab116f580ad3a0cf343402229fc6ed88d29
Kalea Palmer <kaleapalmer@gmail.com>
2018-03-28 18:13:19 -0400

    got command line to work

--- genprime.c
+++ genprime.c
@@ -2,33 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 
-int N; //number
-int t; //threads
-
-void get_input(number, threads) {
-    N = number;
-    t = threads;
+/*
+void get_input(char *inputs[]) {
+    N = inputs[1];
+    t = inputs[2];
+    printf("the arguments are %s and %s", N, t);
 }
-
+*/
 int main(int argc, char *argv[]) {
+    int N; //number
+    int t; //threads
 
-
-    //TRY RUNNING THIS IS LINUX APPLE IS A LITTLE BITCH
     //check if command line inputs are valid
     if (argc != 2) {
-        printf("the number of arguments it %d\n", argc);
-        printf("the inputs are %d and %d\n", argv[1], argv[2] );
+        printf("the number of arguments is %d\n", argc);
+        printf("the inputs are %s and %s\n", argv[1], argv[2]);
         printf("input: N (2 < N <= 10,000) and t (t < 100)\n");
+        N = strtol(argv[1], NULL, 10);
+        t = strtol(argv[2], NULL, 10);
+        printf("the arguments are %d and %d\n", N, t);
         exit(1);
     }
-
-    //set N and t to global varibles
-    N = argv[0];
-    t = argv[1];
-
-    printf("%d %d", N, t);
-
-
-    
 
 }
