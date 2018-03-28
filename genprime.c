b582328c65d41d402099e149a89b9cbf3d92be6a
Kalea Palmer <kaleapalmer@gmail.com>
2018-03-28 18:34:22 -0400

    set a prime array

--- genprime.c
+++ genprime.c
@@ -2,26 +2,47 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*
-void get_input(char *inputs[]) {
-    N = inputs[1];
-    t = inputs[2];
-    printf("the arguments are %s and %s", N, t);
-}
-*/
-int main(int argc, char *argv[]) {
-    int N; //number
-    int t; //threads
+int N; //number
+int t; //threads
+int *prime; //array to hold prime numbers
 
-    //check if command line inputs are valid
-    if (argc != 2) {
-        printf("the number of arguments is %d\n", argc);
-        printf("the inputs are %s and %s\n", argv[1], argv[2]);
-        printf("input: N (2 < N <= 10,000) and t (t < 100)\n");
-        N = strtol(argv[1], NULL, 10);
-        t = strtol(argv[2], NULL, 10);
-        printf("the arguments are %d and %d\n", N, t);
-        exit(1);
+void get_inputs(int argc, char *argv[]);
+
+
+int main(int argc, char *argv[]) {
+
+    //check and set the command line inputs
+    get_inputs(argc, argv);
+
+    //set an array equal to N elements
+
+
+   for (int i = 0; i < N; i++) {
+        printf("the index and number is %d and %d", i, prime[i]);
     }
 
+}
+
+
+
+
+
+
+
+
+void get_inputs(int argc, char *argv[]){
+    if (argc != 3) {
+        printf("input: N (2 < N <= 10,000) and t (t < 100)\n");
+        exit(1);
+    }
+    //assign command line argument to varibles
+    N = strtol(argv[1], NULL, 10);
+    t = strtol(argv[2], NULL, 10);
+    printf("N is %d and t is %d\n", N, t);
+
+    //allocate prime array and set numbers
+    prime = (int*)malloc(N * sizeof(int));
+    for (int i = 0; i < N; i++) {
+        prime[i] = i + 1;
+    }
 }
